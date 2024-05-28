03b69ce16c91db171317d25c053f18fed9059e81
soltonoev_is <soltonoev_is@TSI-L1-25>
2024-05-28 12:06:44 +0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            MyHashTable hs = new MyHashTable(3);
+            hs.Add("sest3", "3");
+            hs["sest3"] = "5";
+        }
+    }
+
+    public class MyHashTable
+    {
+        LinkedList<Item>[] _buckets;
+
+        int _capacity;
+        int _count;
+
+        public MyHashTable(int size)
+        {
+            _capacity = size;
+            _count = 0;
+            _buckets = new LinkedList<Item>[_capacity];
+
+            for (int i = 0; i < _capacity; i++)
+            {
+                _buckets[i] = new LinkedList<Item>();
+            }
+        }
+
+        public string this[string key]
+        {
+            get
+            {
+                return GetByKey(key);
+            }
+            set
+            {
+                SetByKey(key, value);
+            }
+        }
+
+        public void Add(string key, string value)
+        {
+            IncreaseCapacity();
+            if (ContainsKey(key))
+            {
+                throw new ArgumentException("key already exists");
+            }
+            var item = new Item(key, value);
+            int index = GetBucketIndex(key);
+            _buckets[index].AddLast(item);
+            _count++;
+        }
+
+        public void SetByKey(string key, string value)
+        {
+            int index = GetBucketIndex(key);
+            foreach (var item in _buckets[index])
+            {
+                if (item.key == key)
+                {
+                    item.value = value;
+                }
+            }
+        }
+
+        public bool ContainsKey(string key)
+        {
+            int index = GetBucketIndex(key);
+            foreach (var item in _buckets[index])
+            {
+                if (item.key == key)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public string GetByKey(string key)
+        {
+            int index = GetBucketIndex(key);
+            foreach (var item in _buckets[index])
+            {
+                if (item.key == key)
+                {
+                    return item.value;
+                }
+            }
+            return null;
+        }
+
+        private void IncreaseCapacity()
+        {
+            if (_count >= _capacity)
+            {
+                _capacity *= 2;
+                var temp = new LinkedList<Item>[_capacity];
+                foreach (var bucket in _buckets)
+                {
+                    foreach (var item in bucket)
+                    {
+                        int index = GetBucketIndex(item.key);
+                        if (temp[index] == null)
+                        {
+                            temp[index] = new LinkedList<Item>();
+                        }
+                        temp[index].AddLast(item);
+                    }
+                }
+            }
+        }
+
+        public int GetBucketIndex(string key)
+        {
+            return HashFunc(key) % _capacity;
+        }
+
+        public int HashFunc(string key)
+        {
+            int hashCode = 0;
+            foreach (char c in key)
+            {
+                hashCode += c;
+            }
+            return hashCode;
+        }
+    }
+
+    class Item
+    {
+        public string key;
+        public string value;
+
+        public Item(string key, string value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
